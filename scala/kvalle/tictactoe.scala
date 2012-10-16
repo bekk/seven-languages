b9c69d797a8866bb9a1796f94eb4b32a0a16d0eb
Kjetil Valle <kjetil.valle@gmail.com>
2012-10-16 22:32:19 +0200

    rows working

--- scala/kvalle/tictactoe.scala
+++ scala/kvalle/tictactoe.scala
@@ -4,43 +4,59 @@ object CellValue extends Enumeration {
   type CellValue = Value
   val X = Value("x")
   val O = Value("o")
-  val EMPTY = Value("-")
+  val BLANK = Value("-")
 }
 
 import CellValue._
 
 class Board {
 
-  val board = MutableList.fill(9)(EMPTY)
+  val board = MutableList.fill(9)(BLANK)
   
-  def setCell(index: Int, value: CellValue) {
-    board.update(index, value)
+  def setCell(index: Int, value: CellValue) = board.update(index, value)
+  def done() = !board.contains(BLANK) || winner
+  def rows() = board.grouped(3)
+
+  def winner() : Boolean = {
+    val lines = rows
+    lines.foreach { row => 
+      if (row.distinct.size == 1 && !row.contains(BLANK)) {
+        println("We have a winner: " + row.first)
+        return true
+      }
+    }
+    return false
   }
 
   def print() {
     println
-    board.grouped(3).foreach { line =>
+    rows.foreach { line =>
       line.foreach { cell => Predef.print(" " + cell) }
       println
     }
+    println
   }
 
-  def done() : Boolean = !board.contains(EMPTY)
 
 }
 
-println("Cell positions: ")
-println
-println(" 0 1 2")
-println(" 3 4 5")
-println(" 6 7 8")
+def printPositions() {
+  println("Cell positions: \n")
+  println(" 0 1 2")
+  println(" 3 4 5")
+  println(" 6 7 8\n")
+}
 
+// PLAY:
 
+printPositions
+
+var player = X
 val b = new Board()
-b.print
-
-b.setCell(4, X)
-b.print
-
-println
-println(b.done)
+while(!b.done) {
+  print("Select position: ")
+  val pos = readLine().toInt
+  b.setCell(pos, player)
+  player = if (player == X) O else X
+  b.print
+}
