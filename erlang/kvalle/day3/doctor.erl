a33a1f6fdfd199e909c967ea4d92c79b8314bba5
Kjetil Valle <kjetil.valle@gmail.com>
2012-11-26 23:02:40 +0100

    refaktorering

--- erlang/kvalle/day3/doctor.erl
+++ erlang/kvalle/day3/doctor.erl
@@ -1,8 +1,5 @@
-% Doctor module implemented as gen_server
-
 -module(doctor).
 -behaviour(gen_server).
-
 -export([
     start_link/0,
     init/1, 
@@ -14,31 +11,39 @@
     stop/0
 ]).
 
-stop() ->
-    gen_server:cast(doctor, stop).
+stop() -> gen_server:cast(doctor, stop).
+
+% -- Callback functions
 
 start_link() ->
     gen_server:start_link({local, doctor}, doctor, [], []).
 
 init(_Args) ->
     process_flag(trap_exit, true), 
-    io:format("Starting new roulette game.~n"),
-    register(revolver, spawn_link(fun roulette:loop/0)),
+    gen_server:cast(doctor, new_game),
     {ok, 0}.
 
 handle_call(_Message, _From, State) -> {noreply, 0, State}.
 
-handle_cast(stop, State) -> {stop, normal, State};
-handle_cast(_Message, State) -> {noreply, State}.
+handle_cast(Message, State) -> 
+    case Message of
+        new_game ->
+            io:format("Starting new roulette game.~n"),
+            register(revolver, spawn_link(fun roulette:loop/0)),
+            {noreply, State};
+        stop ->
+            {stop, normal, State};
+        _ -> 
+            {noreply, State}
+    end.
 
-handle_info({'EXIT', From, Reason}, _State) -> 
-    io:format("Player ~p died. (~p)~n", [From, Reason]),
-    io:format("Starting new roulette game.~n"),
-    register(revolver, spawn_link(fun roulette:loop/0)),
-    {noreply, 0}.
+handle_info({'EXIT', From, _Reason}, State) -> 
+    io:format("Player ~p died.~n", [From]),
+    gen_server:cast(doctor, new_game),
+    {noreply, State}.
 
-terminate(_Reason, _State) -> 
-    io:format("Stopping: ~p~n", [_Reason]), ok.
+terminate(Reason, _State) -> 
+    io:format("Stopping: ~p~n", [Reason]), ok.
 
 code_change(_OldVersion, State, _Extra) -> 
     io:format("I have changed!~n"),
