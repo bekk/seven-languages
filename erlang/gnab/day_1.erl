cc497dfe1548df2d847d9b14f3281561f3910597
Ole Petter Bang <olepbang@gmail.com>
2012-11-28 17:47:23 +0100

    Add success_or_error solution.

--- erlang/gnab/day_1.erl
+++ erlang/gnab/day_1.erl
@@ -2,6 +2,7 @@
 -export([word_count/1]).
 -export([count_to_ten/0]).
 -export([count_to_ten/1]).
+-export([success_or_error/1]).
 
 word_count([]) -> 0;
 word_count([32|Rest]) -> word_count(Rest);
@@ -13,3 +14,6 @@ word_count([_|Rest]) -> word_count(Rest).
 count_to_ten() -> count_to_ten(1).
 count_to_ten(10) -> io:format("10~n");
 count_to_ten(N) -> io:format("~p~n", [N]), count_to_ten(N+1).
+
+success_or_error({error, Message}) -> io:format("error: ~p~n", [Message]);
+success_or_error(success) -> io:format("success~n").
