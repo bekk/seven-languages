93b73aaa73131e48c80a3d4c36cc536ee7071a5b
Ole Petter Bang <olepbang@gmail.com>
2012-11-28 17:43:51 +0100

    Add count_to_ten solution.

--- erlang/gnab/day_1.erl
+++ erlang/gnab/day_1.erl
@@ -1,5 +1,7 @@
 -module(day_1).
 -export([word_count/1]).
+-export([count_to_ten/0]).
+-export([count_to_ten/1]).
 
 word_count([]) -> 0;
 word_count([32|Rest]) -> word_count(Rest);
@@ -7,3 +9,7 @@ word_count([_|[32|Rest]]) -> 1 + word_count(Rest);
 word_count([32]) -> 0;
 word_count([_]) -> 1;
 word_count([_|Rest]) -> word_count(Rest).
+
+count_to_ten() -> count_to_ten(1).
+count_to_ten(10) -> io:format("10~n");
+count_to_ten(N) -> io:format("~p~n", [N]), count_to_ten(N+1).
