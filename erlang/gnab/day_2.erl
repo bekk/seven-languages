ba79975afab6ac3cdc705afc1b54f052a655ee6f
Ole Petter Bang <olepbang@gmail.com>
2012-11-28 18:06:02 +0100

    Add sum solution.

--- erlang/gnab/day_2.erl
+++ erlang/gnab/day_2.erl
@@ -1,6 +1,10 @@
 -module(day_2).
 -export([filter_by/2]).
+-export([sum/1]).
 
 filter_by([], _) -> false;
 filter_by([{Language, Text}|_], Language) -> Text;
 filter_by([{_, _}|Rest], Language) -> filter_by(Rest, Language).
+
+sum([]) -> false;
+sum(Items) -> [{Item, Quantity * Price} || {Item, Quantity, Price} <- Items].
